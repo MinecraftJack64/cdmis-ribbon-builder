ed2d453dcbe63eba24c6bd6bebd3b8361baca99c
MinecraftJack64 <48302222+MinecraftJack64@users.noreply.github.com>
2025-02-18 22:50:19 -0800

    Extension stop behavior
    Extension now reveals data when done instead of crashing

--- webextension/index.js
+++ webextension/index.js
@@ -11,7 +11,21 @@ function onListExpand(){
     }
     let table = document.getElementById("ctl00_mainContent_gvCadets").children[0];
     let tr = table.children[currentcadet];
-    let name = tr.children[0].children[0].innerHTML+"_"+tr.children[1].children[0].innerHTML;
+    try{
+        let name = tr.children[0].children[0].innerHTML+"_"+tr.children[1].children[0].innerHTML;
+    }catch(e){
+        let d = document.createElement("textarea");
+        chrome.storage.local.get('ribs', function(data) {
+            d.innerHTML = JSON.stringify(data.ribs);
+            d.id = "finalribs";
+            d.style.width = "100%";
+            d.style.height = "300px";
+            let i = document.createElement("h2");
+            i.innerHTML = "ALL CADET RIBBON DATA OBTAINED, copy the text below and paste it into the all.json file under the cdmisdata folder";
+            document.getElementById("content").prepend(i, d);//DONE
+        });
+        return;
+    }
     console.log(name);
 
     //increment local storage cadet number by 1 and set cadet name
